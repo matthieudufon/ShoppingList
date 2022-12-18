409526f046f5d8dcdbda6085fe0dcf495c0f449b
matthieuclam <matthieu.dufon@gmail.com>
2022-12-18 18:59:43 +0100

    Add feature to insert new Aliments in DB

--- app/src/main/java/fr/utt/if26/shoppinglist/AlimentsFragment.java
+++ app/src/main/java/fr/utt/if26/shoppinglist/AlimentsFragment.java
@@ -10,16 +10,31 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import fr.utt.if26.shoppinglist.adapters.AlimentListAdapter;
+import fr.utt.if26.shoppinglist.entities.AlimentEntity;
 import fr.utt.if26.shoppinglist.viewModels.AlimentViewModel;
 
 
-public class AlimentsFragment extends Fragment {
+public class AlimentsFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
-    RecyclerView recyclerView;
+    private RecyclerView recyclerView;
+    private EditText editTextNom;
+    private Spinner spinnerCategorie;
+    private ImageButton validateButton;
 
     private static AlimentViewModel alimentViewModel;
+
+    private List<String> spinnerOptions = new ArrayList<String>();
+    private String spinnerChoice = "Céréales";
 
     public AlimentsFragment() {
 
@@ -29,6 +44,10 @@ public class AlimentsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View result = inflater.inflate(R.layout.fragment_aliments, container, false);
 
+        editTextNom = result.findViewById(R.id.fragment_aliments_et);
+        spinnerCategorie = result.findViewById(R.id.fragment_aliments_spinner);
+        validateButton = result.findViewById(R.id.fragment_aliments_ib);
+
         recyclerView = result.findViewById(R.id.fragment_aliments_rv);
         final AlimentListAdapter adapter = new AlimentListAdapter(new AlimentListAdapter.AlimentDiff());
         recyclerView.setAdapter(adapter);
@@ -37,6 +56,31 @@ public class AlimentsFragment extends Fragment {
         alimentViewModel = new ViewModelProvider(this).get(AlimentViewModel.class);
         alimentViewModel.getAllAliments().observe(getViewLifecycleOwner(), adapter::submitList);
 
+        spinnerOptions.add("Céréales");
+        spinnerOptions.add("Boissons");
+        spinnerOptions.add("Viande et poisson");
+        spinnerOptions.add("Légumes et fruits");
+        spinnerOptions.add("Produits sucrés");
+        spinnerOptions.add("Produits laitiers");
+        spinnerOptions.add("Plats composés");
+        spinnerOptions.add("Autre");
+        spinnerCategorie.setOnItemSelectedListener(this);
+
+        ArrayAdapter spinnerAdapter = new ArrayAdapter(result.getContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, spinnerOptions);
+        spinnerAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        spinnerCategorie.setAdapter(spinnerAdapter);
+
+        validateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!editTextNom.getText().toString().equals("")) {
+                    AlimentEntity aliment = new AlimentEntity(editTextNom.getText().toString(), spinnerChoice);
+                    alimentViewModel.insert(aliment);
+                    editTextNom.setText("");
+                }
+            }
+        });
+
         return result;
     }
 
@@ -44,4 +88,12 @@ public class AlimentsFragment extends Fragment {
         return alimentViewModel;
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        spinnerChoice = spinnerOptions.get(i);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+    }
 }
